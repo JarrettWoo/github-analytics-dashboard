7b8f691562021d7838eb4d0bb29de0ca639d67ed
Jarrett Woo <jarrwoo98@gmail.com>
2026-02-17 10:42:14 -0500

    Initial project setup with Vite, React, TS, adn Tailwind

---
+++ src/types/github.ts
@@ -0,0 +1,29 @@
+export interface Repository {
+    id: number;
+    name: string;
+    full_name: string;
+    description: string | null;
+    stargazers_count: number;
+    forks_count: number;
+    watchers_count: number;
+    open_issues_count: number;
+    language: string | null;
+    owner: {
+        login: string;
+        avatar_url: string;
+    };
+    
+}
+    export interface ComitActivity {
+        total: number;
+        week: number;
+        days: number[];
+    }
+
+    export interface Contributor {
+        login: string;
+        avatar_url: string;
+        contributions: number;
+        html_url: string;
+    }
+    export type Languages = Record<string, number>;
